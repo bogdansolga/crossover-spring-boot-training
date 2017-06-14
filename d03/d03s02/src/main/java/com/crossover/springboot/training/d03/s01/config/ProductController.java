cc696f120d0345fc544cdc75bb0852f529448b08
Bogdan Solga <bogdan.solga@gmail.com>
2017-06-14 11:03:25 +0300

    [dev] Added a small sample on how to use the @AuthenticationPrincipal annotation

--- d03/d03s02/src/main/java/com/crossover/springboot/training/d03/s01/config/ProductController.java
+++ d03/d03s02/src/main/java/com/crossover/springboot/training/d03/s01/config/ProductController.java
@@ -2,18 +2,23 @@ package com.crossover.springboot.training.d03.s01.config;
 
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 public class ProductController {
 
     @PreAuthorize("isFullyAuthenticated() AND hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
-    public void addProduct() {
-
+    public void addProduct(@AuthenticationPrincipal UserDetails userDetails) {
+        final Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
     }
 
-    public void prcrossoverssInternalRequest(@RequestHeader(value = "SECURITY_TOKEN") final String securityToken) {
+    public void processInternalRequest(@RequestHeader(value = "SECURITY_TOKEN") final String securityToken) {
         if (securityToken.isEmpty()) {
             throw new AccessDeniedException("Nope");
         }
